a050c12afac70035782b3f8b4e4e88239ca05cc1
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-07-15 20:19:12 +0200

    Improve connection termination

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ var http = require('http');
 
 var server = http.createServer();
 
-server.on('connect', function(req, res){
-  console.log('connected');
-});
-
-server.on('checkContinue', function(req, res) {
+function handleRequest(req, res){
+  console.log('method', req.method);
   console.log('headers', req.headers)
 
   req.on('data', function(chunk){
     console.log(chunk.toString());
   });
 
+  req.on('end', function(){
+    console.log('=================');
+  });
+}
+
+server.on('request', function(req, res){
+  handleRequest(req, res);
+  res.end();
+});
+
+server.on('checkContinue', function(req, res) {
+  handleRequest(req, res);
   res.writeContinue();
   res.end();
 });
